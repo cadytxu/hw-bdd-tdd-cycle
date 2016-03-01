5f3cecf4cc2cdaafbf3b8a4b95a46670aaef4653
cadytxu <cadytxu@gmail.com>
2016-03-01 21:06:19 +0000

    fixed home page path

---
+++ hw4/config/routes.rb
@@ -0,0 +1,7 @@
+Rottenpotatoes::Application.routes.draw do
+  resources :movies 
+  #map '/' to be a redirect to '/movies'
+  #root :to => redirect('/movies')
+  get 'movies/same_director/:id' => 'movies#same_director', :as => :same_director
+ end  
+  
